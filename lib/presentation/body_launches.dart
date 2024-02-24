1ee83e541c99acc46b1d97e60296be1c5fde82c7
Ladon Skliarov <ladonskliarov@gmail.com>
2024-02-24 17:31:59 +0200

    Some changes

--- lib/presentation/body_launches.dart
+++ lib/presentation/body_launches.dart
@@ -9,6 +9,7 @@ import 'package:space_x_launches/presentation/launch_card.dart';
 class BodyLaunchesWidget extends StatelessWidget {
   ///Constructor
   const BodyLaunchesWidget({super.key});
+  
 
   @override
   Widget build(BuildContext context){
